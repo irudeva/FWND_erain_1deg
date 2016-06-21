e5eede0bd617415482fe493aed6eb355501ebc8b
irudeva <irinarudeva@gmail.com>
2016-06-21 15:46:55 +1000

    start

---
+++ get.erain.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from ecmwfapi import ECMWFDataServer
+server = ECMWFDataServer()
+for yr in range(2013,2016) :
+  server.retrieve({
+    "class": "ei",
+    "dataset": "interim",
+    "date": "%.4d-01-01/to/%.4d-12-31" % (yr,yr),
+    "expver": "1",
+    "grid": "0.75/0.75",
+    "levtype": "sfc",
+    "param": "165.128/166.128",
+    "step": "0",
+    "stream": "oper",
+    "time": "00:00:00/06:00:00/12:00:00/18:00:00",
+    "type": "an",
+    "target": "erain.w10.%.4d.1deg.nc" % (yr),
+    "format": "netcdf"
+  })
